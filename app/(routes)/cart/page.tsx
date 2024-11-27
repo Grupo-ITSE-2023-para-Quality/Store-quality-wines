e8901a3f50bb1b2313d05d258c2c43a79d8fe700
ProfeCeci <andreacecilopez88@gmail.com>
2024-11-27 17:44:19 -0300

    update cart page

--- app/(routes)/cart/page.tsx
+++ app/(routes)/cart/page.tsx
@@ -1,21 +1,19 @@
 "use client";
 
+import { Suspense } from 'react';
 import Container from "@/components/ui/container";
 import useCart from "@/hooks/use-cart";
-
 import CartItem from "./components/cart-item";
 import Summary from "./components/summary";
 import ClientForm from "./components/client-form";
 
-const CartPage = () => {
+function CartPageContent() {
   const cart = useCart();
 
   return (
     <div className="bg-white">
       <Container>
-        <div
-          className="px-4 py-8 sm:px-6 lg:px-8 mt-28"        >
-          {" "}
+        <div className="px-4 py-8 sm:px-6 lg:px-8 mt-28">
           <h1 className="text-3xl font-bold text-black">Carrito de compras</h1>
           <div className="mt-4 lg:grid lg:grid-cols-12 lg:gap-8">
             <div className="lg:col-span-7">
@@ -40,6 +38,12 @@ const CartPage = () => {
       </Container>
     </div>
   );
-};
+}
 
-export default CartPage;
+export default function CartPage() {
+  return (
+    <Suspense fallback={<div>Cargando...</div>}>
+      <CartPageContent />
+    </Suspense>
+  );
+}
